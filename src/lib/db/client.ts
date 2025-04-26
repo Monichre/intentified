9b3d2449819281cd1b867936d321a28788c1d21d
monichre <liamhellis@gmail.com>
2025-04-26 11:36:51 -0500

    fix: build errors

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,17 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-if (!process.env.NEXT_PUBLIC_BLOCKS_SUPABASE_URL) {
-	throw new Error("Missing env.NEXT_PUBLIC_BLOCKS_SUPABASE_URL");
-}
-if (!process.env.BLOCKS_SUPABASE_SERVICE_ROLE_KEY) {
-	throw new Error("Missing env.BLOCKS_SUPABASE_SERVICE_ROLE_KEY");
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+	throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 }
 
 // Likely don't want to do this in production, we use the admin client to create documents and tasks.
 // Using admin here is just to get around forcing users to authenticate before using the preview
 export const supabaseAdminClient = createClient(
-	process.env.NEXT_PUBLIC_BLOCKS_SUPABASE_URL,
-	process.env.BLOCKS_SUPABASE_SERVICE_ROLE_KEY,
+	process.env.NEXT_PUBLIC_SUPABASE_URL,
+	process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_API_KEY || "",
 	{
 		auth: { persistSession: false },
 	},
